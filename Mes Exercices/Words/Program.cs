272fbebd83682c0a528475ef11c0b5a6e801e076
RED-DFRL <ps01nnt@eduvaud.ch>
2025-09-04 11:57:31 +0200

    update(Words): ajout suite exercices

--- Mes Exercices/Words/Program.cs
+++ Mes Exercices/Words/Program.cs
@@ -10,7 +10,7 @@ namespace Words
         {
             string[] words = { "bonjour", "hello", "monde", "vert", "roxuge", "bleu", "jaune" };
             string[] words2 = { "whatThe!!!", "bonjour", "hello", "monde", "vert", "rouge", "bleu", "jaune", "My kingdom for a horse !", "Ooops I did it again" };
-            string[] words3 = { "+++++", "<<<<<", ">>>>>", "bonjour", "hello", "@@@@", "vert", "rouge", "bleu", "jaune", "#####", "%%%%%%%" };
+           
 
             //Fonctions de filtrage
             Func<string, bool> noX = word => !word.Contains("x");
@@ -21,18 +21,22 @@ namespace Words
             Func<string, bool> sameAsAvg =
                 word => word.Length == words.Average(word2 => word2.Length);
 
-            Func<string[], string[]> reverse = word => word.Reverse().ToArray();
-            Func<string[], string[]> sortAsc = word => word.OrderBy(word2 => word2).ToArray();
-            Func<string[], string[]> sortDesc = word => word.OrderByDescending(word2 => word2).ToArray();
+            Func<string[], string[]> reverse = words => words.Reverse().ToArray();
+            Func<string[], string[]> sortAsc = words => words.OrderBy(word => word).ToArray();
+            Func<string[], string[]> sortDesc = words => words.OrderByDescending(word => word).ToArray();
 
             Func<string[], string[]> filtered = word => word.Skip(1).Take(7).ToArray();
 
+            Func<IEnumerable<string>> c = () =>
+            {
+                string[] words3 = { "+++++", "<<<<<", ">>>>>", "bonjour", "hello", "@@@@", "vert", "rouge", "bleu", "jaune", "#####", "%%%%%%%" };
+                var clean = words3.Where(word => word.All(letter => char.IsLetterOrDigit(letter)));
+                return clean;
+            };
 
             string[] sortedAsc = sortAsc(words);
             string[] sortedDesc = sortDesc(words);
             string[] filter = filtered(words2);
-
-            foreach (string s in filter) { Console.WriteLine(s); }
 
             /*//Recueil de fonctions
             var filters = new List<Func<string, bool>>();
