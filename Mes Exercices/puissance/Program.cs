dbe5208a762779f5ef1f7904232308edcf2d5cce
RED-DFRL <ps01nnt@eduvaud.ch>
2025-10-30 08:55:13 +0100

    Feat(exo): ajout puissance

---
+++ Mes Exercices/puissance/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Puissance
+{
+    internal class Program
+    {
+        public static double Power(double baseNumber, int exponent)
+        {
+            if (exponent == 1)
+            {
+                return baseNumber;
+            }
+            return baseNumber * Power(baseNumber, exponent-1);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Power(5,2));
+        }
+    }
+}
